688b14e8aed3ea4792c917e842c18a0611c6ca95
marklukin <mark.m.lukin@gmail.com>
2024-11-05 20:13:35 +0100

    sum

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('Happy developing ✨')
+//for
+const sum = (...args) => {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    return sum;
+}
+
+//for... of
+const sum2 = (...args) => {
+    let sum = 0;
+    for (const item of args) {
+        sum += item;
+    }
+    return sum;
+}
+
+//while
+const sum3 = (...args) => {
+    let sum = 0;
+    let i = 0;
+    while (i < args.length) {
+        sum += args[i];
+        i++;
+    }
+    return sum;
+}
+//do while
+const sum4 = (...args) => {
+    let sum = 0;
+    let i = 0;
+    do {
+        sum += args[i];
+        i++
+    } while (i < args.length);
+    return sum;
+}
+//Array.prototype.reduce()
+ const sum5 = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+ }
+
+ console.log(sum5(1,2,3,4,11));
